8be917f5c5de097ab9371bc429e1018dfcff04aa
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2025-01-11 16:12:48 -0800

    inital nhd0420d3z

---
+++ drivers/include/nhdd3zv3.hpp
@@ -0,0 +1,34 @@
+
+#include <libhal-util/serial.hpp>
+#include <libhal/i2c.hpp>
+#include <libhal/units.hpp>
+namespace sjsu::drivers {
+
+class nhdd3zv3
+{
+private:
+  constexpr static hal::byte default_i2c_address = 0x50;
+  constexpr static hal::byte display_lines = 4;
+  constexpr static hal::byte display_columns = 20;
+
+  hal::byte coordinates_to_position(hal::byte line, hal::byte column);
+
+  void write_char(char c);
+  void set_cursor_position(hal::byte line, hal::byte column);
+  void move_cursor_right();
+  void home_cursor();
+  void clear_screen();
+
+  hal::i2c& m_i2c_bus;
+  hal::byte m_i2c_address;
+  hal::byte m_cursor_line = 0;
+  hal::byte m_cursor_column = 0;
+
+public:
+  nhdd3zv3(hal::i2c& p_i2c, hal::byte p_i2c_address = default_i2c_address);
+
+  void display_message(std::string_view str);
+  void power(bool on);
+};
+
+}  // namespace sjsu::drivers

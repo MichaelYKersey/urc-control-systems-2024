e2d60e9efa2e228f9ada8caf2c82d62b17fc5ee0
Vincent Wu <chungxuan.wu@gmail.com>
2025-05-01 16:11:18 -0700

    end of date changes

--- arm/conanfile.py
+++ arm/conanfile.py
@@ -4,10 +4,12 @@ required_conan_version = ">=2.0.14"
 
 
 class demos(ConanFile):
-    python_requires = "libhal-bootstrap/[^2.1.1]"
+    python_requires = "libhal-bootstrap/[^4.1.0]"
     python_requires_extend = "libhal-bootstrap.demo"
 
     def requirements(self):
         bootstrap = self.python_requires["libhal-bootstrap"]
         bootstrap.module.add_demo_requirements(self)
         self.requires("libhal-canrouter/[^3.0.0]")
+        # self.requires("libhal-arm-mcu/[1.9.0 || latest]")
+        self.requires("minimp3/cci.20211201")
--- arm/platforms/stm32f103c8.cpp
+++ arm/platforms/stm32f103c8.cpp
@@ -3,34 +3,38 @@
 #include <libhal-arm-mcu/stm32f1/output_pin.hpp>
 #include <libhal-arm-mcu/stm32f1/pin.hpp>
 #include <libhal-arm-mcu/stm32f1/uart.hpp>
-#include <libhal-micromod/micromod.hpp>
+#include <libhal-arm-mcu/system_control.hpp>
 namespace sjsu::arm {
 
 hardware_map_t initialize_platform()
 {
   using namespace hal::literals;
 
-  hal::micromod::v1::initialize_platform();
   static hal::stm32f1::uart uart1(hal::port<1>,
                                   hal::buffer<1024>,
                                   hal::serial::settings{
                                     .baud_rate = 9600,
                                   });
-  static hal::stm32f1::output_pin a_high('B', 3);  // INHC
-  static hal::stm32f1::output_pin b_high('B', 4);  // INHB
-  static hal::stm32f1::pwm a_low('A', 8);          // INLC
-  static hal::stm32f1::pwm b_low('A', 0);          // INLB
+  static hal::stm32f1::output_pin p_a_high('B', 3);  // INHC
+  static hal::stm32f1::output_pin p_b_high('B', 4);  // INHB
+  static hal::stm32f1::pwm p_a_low('A', 8);          // INLC
+  static hal::stm32f1::pwm p_b_low('A', 0);          // INLB
+  hal::pwm16_channel* pwm_channel = nullptr;
+  hal::pwm_group_manager* pwm_frequency = nullptr;
   static hal::stm32f1::output_pin led('C', 13);
-
+  auto cpu_frequency = hal::stm32f1::frequency(hal::stm32f1::peripheral::cpu);
+  static hal::cortex_m::dwt_counter steady_clock(cpu_frequency);
+  hal::stm32f1::maximum_speed_using_internal_oscillator();
+  hal::stm32f1::release_jtag_pins();
   return {
     .led = &led,
     .console = &uart1,
-    .clock = &hal::stm32f1::v1::uptime_clock(),
-    .reset = +[]() { hal::stm32f1::v1::reset(); },
-    .a_low = &a_low,
-    .b_low = &b_low,
-    .a_high = &a_high,
-    .b_high = &b_high,
+    .clock = &steady_clock,
+    .reset = +[]() { hal::cortex_m::reset(); },
+    .a_low = &p_a_low,
+    .b_low = &p_b_low,
+    .a_high = &p_a_high,
+    .b_high = &p_b_high,
   };
 }
 }  // namespace sjsu::arm

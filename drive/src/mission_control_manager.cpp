d7375859ee99dd842a8ebb079617f6edeab89eb6
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2025-11-15 13:40:53 -0800

    more implementation

--- drive/src/mission_control_manager.cpp
+++ drive/src/mission_control_manager.cpp
@@ -1,11 +1,14 @@
+#include "swerve_module.hpp"
 #include <array>
 #include <bit>
 #include <cstdint>
 #include <libhal-util/bit.hpp>
+#include <libhal-util/can.hpp>
 #include <libhal/can.hpp>
 #include <libhal/units.hpp>
 #include <mission_control_manager.hpp>
 #include <optional>
+#include <span>
 #include <sys/_types.h>
 #include <sys/types.h>
 
@@ -13,17 +16,17 @@ namespace {
 enum class can_message_id : uint32_t
 {
   set_chassis_velocities,
-  set_velocities_Response,
+  set_chassis_velocities_reply,
   heartbeat,
   heartbeat_reply,
   homing_sequence,
-  reply_homing_sequence,
+  homing_sequence_reply,
   get_offset,
-  return_offset,
-  get_Estimated_velocities,
-  return_estimated_chassis_velocities,
+  get_offset_reply,
+  get_estimated_velocities,
+  get_estimated_velocities_reply,
   config,
-  Config_ack,
+  config_ack,
 };
 }
 
@@ -45,7 +48,8 @@ mission_control_manager::mission_control_manager(
 {
 }
 
-float fixed_point_to_float(int16_t p_fixed_point_num, int p_expo)
+float mission_control_manager::fixed_point_to_float(int16_t p_fixed_point_num,
+                                                    int p_expo)
 {
   unsigned int shifted_expo = 1 << abs(p_expo);
   float floating_num = p_fixed_point_num;
@@ -55,7 +59,9 @@ float fixed_point_to_float(int16_t p_fixed_point_num, int p_expo)
     return floating_num * shifted_expo;
   }
 }
-int16_t float_to_fixed_point(float p_floating_point_num, int p_expo)
+int16_t mission_control_manager::float_to_fixed_point(
+  float p_floating_point_num,
+  int p_expo)
 {
   unsigned int shifted_expo = 1 << abs(p_expo);
   if (p_expo > 0) {
@@ -64,7 +70,7 @@ int16_t float_to_fixed_point(float p_floating_point_num, int p_expo)
     p_floating_point_num *= shifted_expo;
   }
   // TODO: consider implementing rounding
-  return p_floating_point_num;
+  return static_cast<int16_t>(p_floating_point_num);
 }
 
 int16_t array_to_int16(std::array<hal::byte, 2> const& p_array);
@@ -77,14 +83,82 @@ std::array<hal::byte, 2> int16_to_byte_array(int16_t p_num)
 }
 
 // returns most recent velocity request
-// std::optional<chassis_velocities_request>
-// mission_control_manager::read_set_velocity_request();
-// void mission_control_manager::reply_set_velocity_reques?t(
-//   chassis_velocities_request const& p_chassis_vel);
+std::optional<chassis_velocities_request>
+mission_control_manager::read_set_velocity_request()
+{
+  std::optional<hal::can_message> velocity_request_message;
+  while (true) {
+    std::optional<hal::can_message> check_velocity_request_message =
+      m_set_chassis_velocities_message_finder.find();
+    if (check_velocity_request_message) {
+      if (check_velocity_request_message->length == 7) {
+        velocity_request_message = check_velocity_request_message;
+      }
+    } else {
+      break;
+    }
+  }
+  if (not velocity_request_message) {
+    return std::nullopt;
+  }
+  chassis_velocities velocities;
+  auto& payload = velocity_request_message->payload;
+  velocities.translation.x =
+    fixed_point_to_float(array_to_int16({ payload[0], payload[1] }), -12);
+  velocities.translation.x =
+    fixed_point_to_float(array_to_int16({ payload[2], payload[3] }), -12);
+  velocities.rotational_vel =
+    fixed_point_to_float(array_to_int16({ payload[4], payload[5] }), -6);
+  chassis_velocities_request cvr = { .chassis_vels = velocities,
+                                     .module_conflicts =
+                                       (bool)(payload[6] & 0x01) };
+  return cvr;
+}
+
+void mission_control_manager::reply_set_velocity_request(
+  chassis_velocities_request const& p_chassis_vel)
+{
+  auto x_vel_array = int16_to_array(
+    float_to_fixed_point(p_chassis_vel.chassis_vels.translation.x, -12));
+  auto y_vel_array = int16_to_array(
+    float_to_fixed_point(p_chassis_vel.chassis_vels.translation.y, -12));
+  auto rot_vel_array = int16_to_array(
+    float_to_fixed_point(p_chassis_vel.chassis_vels.rotational_vel, -6));
+  hal::can_message reply{ .id = static_cast<uint32_t>(
+                            can_message_id::set_chassis_velocities_reply),
+                          .length = 7,
+                          .payload = { x_vel_array[0],
+                                       x_vel_array[1],
+                                       y_vel_array[0],
+                                       y_vel_array[1],
+                                       rot_vel_array[0],
+                                       rot_vel_array[1],
+                                       p_chassis_vel.module_conflicts } };
+  m_can_transceiver->send(reply);
+}
 
 // return true if homing requested;
-// bool mission_control_manager::read_homing_request();
-// void mission_control_manager::reply_homing_request();
+bool mission_control_manager::read_homing_request()
+{
+  bool requested = false;
+  while (true) {
+    std::optional<hal::can_message> message = m_heartbeat_message_finder.find();
+    if (not message) {
+      if (message->length == 0) {
+        requested = true;
+      }
+    } else {
+      return requested;
+    }
+  }
+}
+
+void mission_control_manager::reply_homing_request()
+{
+  m_can_transceiver->send(
+    { .id = static_cast<uint32_t>(can_message_id::homing_sequence_reply),
+      .length = 0 });
+}
 
 void mission_control_manager::fulfill_data_requests(
   drivetrain const& p_drivetrain)
@@ -94,7 +168,7 @@ void mission_control_manager::fulfill_data_requests(
   std::optional<hal::can_message> offset_request_message;
   do {
     offset_request_message = m_get_offset_message_finder.find();
-    if (offset_request_message) {
+    if (offset_request_message && offset_request_message->length == 1) {
       offsets_requested |= 1 << offset_request_message->payload[0];
     }
   } while (offset_request_message);
@@ -117,9 +191,50 @@ void mission_control_manager::fulfill_data_requests(
     }
     i++;
   }
+  // TODO: implement once chassis estimate velocity calculations are implemented
+  while (true) {
+    auto get_request = m_get_chassis_velocities_message_finder.find();
+    if (get_request) {
+      if (get_request->length == 0) {
+        chassis_velocities velocity_estimate =
+          p_drivetrain.get_state_estimate();
+        auto x_vel_array = int16_to_array(
+          float_to_fixed_point(velocity_estimate.translation.x, -12));
+        auto y_vel_array = int16_to_array(
+          float_to_fixed_point(velocity_estimate.translation.y, -12));
+        auto rot_vel_array = int16_to_array(
+          float_to_fixed_point(velocity_estimate.rotational_vel, -6));
+        hal::can_message reply{ .id = static_cast<uint32_t>(
+                                  can_message_id::set_chassis_velocities_reply),
+                                .length = 6,
+                                .payload = { x_vel_array[0],
+                                             x_vel_array[1],
+                                             y_vel_array[0],
+                                             y_vel_array[1],
+                                             rot_vel_array[0],
+                                             rot_vel_array[1] } };
+        // clear remaining requests
+        while (m_get_chassis_velocities_message_finder.find())
+          ;
+        m_can_transceiver->send(reply);
+        break;
+      }
+    } else {
+      break;
+    }
+  }
 }
 
-// void mission_control_manager::clear_heartbeat_requests();
-// void mission_control_manager::reply_heartbeat();
+void mission_control_manager::clear_heartbeat_requests()
+{
+  while (m_heartbeat_message_finder.find())
+    ;
+}
+void mission_control_manager::reply_heartbeat()
+{
+  m_can_transceiver->send(
+    { .id = static_cast<uint32_t>(can_message_id::heartbeat_reply),
+      .length = 0 });
+}
 
 }  // namespace sjsu::drive

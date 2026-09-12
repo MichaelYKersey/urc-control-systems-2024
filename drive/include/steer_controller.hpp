65c1d547d4a20bcbbbc870d830277916a9316611
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2026-09-12 14:59:32 -0700

    pr changes 2

--- drive/include/steer_controller.hpp
+++ drive/include/steer_controller.hpp
@@ -1,9 +1,12 @@
 #pragma once
 #include <libhal/units.hpp>
-class steer_controller {
+class steer_controller
+{
 public:
+  virtual ~steer_controller();
+
   virtual void stop() = 0;
-  
+
   virtual void hard_home() = 0;
   virtual void home() = 0;
   virtual void home_periodic() = 0;
@@ -13,5 +16,4 @@ public:
   virtual void set_target_position(hal::degrees p_target_position) = 0;
   virtual hal::degrees get_target_postion() = 0;
   virtual hal::degrees get_actual_postion() = 0;
-  
 };
